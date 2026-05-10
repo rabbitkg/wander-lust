93a780e0aad64eb18b9b4812200bba44e1fca304
rabbitkg <rabbitkgbd@gmail.com>
2026-05-10 23:05:21 +0600

    added navber, banner and footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between shadow-md p-4 bg-white sticky top-0 z-50">
+            <ul className="flex gap-3">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/destinations">Destinations</Link></li>
+                <li><Link href="/my-bookings">My Bookings</Link></li>
+            </ul>
+
+            <div>
+                <Image src={'/assets/Wanderlast.png'} alt="Navbar logo" width={150} height={150} />
+            </div>
+            <ul className="flex gap-3">
+                <li><Link href="/profile">Profile</Link></li>
+                <li><Link href="/login">Login</Link></li>
+                <li><Link href="/signup">Sign Up</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
